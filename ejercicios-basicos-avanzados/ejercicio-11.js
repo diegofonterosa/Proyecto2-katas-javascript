660c5db47699b027d0f39b1a64fdd7fecaa043e1
diego <diegofonterosa@gmail.com>
2025-12-18 09:45:32 +0100

    Ejercicios 8,9,10,11 18/12/2025

---
+++ ejercicios-basicos-avanzados/ejercicio-11.js
@@ -0,0 +1,28 @@
+const mixedElements = [
+    6,
+    1,
+    "Marvel",
+    1,
+    "hamburguesa",
+    "10",
+    "Prometeo",
+    8,
+    "Hola Mundo",
+];
+
+function averageWord(list) {
+    let suma = 0;
+
+    for (let i= 0; i < list.length; i++) {
+        if (typeof list[i] === "number") {
+            suma += list[i];
+        } else if (typeof list[i] === "string") {
+            suma += list[i].length;
+        }
+    }
+
+    return suma / list.length;
+}
+
+console.log(averageWord(mixedElements));
+
